3e0e0137d915f912415ebaddf14549a9711f05f2
TimeTGame <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>
2025-02-14 21:36:42 +0300

    A keyboard has been made for working with files

--- app/keyboards.py
+++ app/keyboards.py
@@ -3,6 +3,12 @@ from aiogram.types import (ReplyKeyboardMarkup, KeyboardButton,
 from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
 
 main = ReplyKeyboardMarkup(keyboard=[
-    [KeyboardButton(text='Change directory', callback_data='move')],
-    [KeyboardButton(text='Block screen', callback_data='block'), KeyboardButton(text='Take a photo', callback_data='photo')]
+    [KeyboardButton(text='Work with files', callback_data='files')],
+    [KeyboardButton(text='Security', callback_data='security')]
+], resize_keyboard=True)
+
+kb_files = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text='Change directory', callback_data='move')],
+    [InlineKeyboardButton(text='List of files', callback_data='filesList')],
+    [KeyboardButton(text='Create', callback_data='create'), KeyboardButton(text='Delete', callback_data='delete')]
 ], resize_keyboard=True)
